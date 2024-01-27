936cdb57590f1f8d11f14fdea44491049dac0d6f
TaurusA <realtaurus97@gmail.com>
2024-01-27 14:40:44 -0600

    Select statements 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is ok.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science huh, ok Einstein.");
+                    break;
+                case "english":
+                    Console.WriteLine("English breaks its on rules all the time.");
+                    break;
+                case "history":
+                    Console.WriteLine("History is the best subject.");
+                    break;
+                case "art":
+                    Console.WriteLine("Art, I don't see why it's a mandatory class.");
+                    break;
+                default:
+                    Console.WriteLine($"{favSubject} is not what I expected but keep learning.");
+                    break;
+            }
         }
     }
 }
